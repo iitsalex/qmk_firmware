f420a7dd48f6f3ce892113d4a2c81aa6b53d3b48
Alex Khalilieh <iitsalex@hotmail.com>
2020-05-04 18:22:36 -0700

    more weird changes and testing
    lower arrows keys + home end relocation

--- keyboards/planck/keymaps/iitsalex/keymap.c
+++ keyboards/planck/keymaps/iitsalex/keymap.c
@@ -102,18 +102,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   ~  |   !  |   @  |   #  |   $  |   %  |   ^  |   &  |   *  |   (  |   )  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
+ * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   _  |   +  |   {  |   }  |  \   |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   _  |   +  |   {  |   }  |      |
+ * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |   _  |   +  |  END | SHFT |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_LOWER] = LAYOUT_planck_grid(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,    KC_ASTR,    KC_LPRN, KC_RPRN, KC_BSPC,
-    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS,    KC_EQL,     KC_LBRC, KC_RBRC, KC_PIPE,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_UNDS,    KC_PLUS,    KC_LCBR, KC_RCBR, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______,    KC_MNXT,    KC_VOLD, KC_VOLU, KC_MPLY
+    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR,  KC_ASTR,  KC_LPRN, KC_RPRN, KC_BSPC,
+    KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_UNDS,  KC_PLUS,  KC_LCBR, KC_RCBR, KC_PIPE,
+    KC_LSFT, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_RSFT,  KC_HOME,  KC_UP,   KC_END, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______,  KC_LEFT,  KC_DOWN, KC_RGHT, KC_MPLY
 ),
 
 /* Raise
@@ -130,7 +130,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_planck_grid(
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
     KC_DEL,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, _______,
+    _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HOME, KC_END,  _______, KC_RSFT, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY
 ),
 
@@ -148,9 +148,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_FUNLAYER] = LAYOUT_planck_grid(
     KC_TAB,  KC_7,    KC_8,    KC_9,    _______, _______, _______, KC_HOME, KC_UP,   KC_END,  _______, KC_BSPC,
-    KC_ESC,  KC_4,    KC_5,    KC_6,    _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_RSFT, KC_DEL,
+    KC_DEL,  KC_4,    KC_5,    KC_6,    _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_RSFT, KC_DEL,
     KC_LSFT, KC_1,    KC_2,    KC_3,    _______, _______, _______, _______, KC_HOME, KC_UP,   KC_END,  KC_ENT,
-    _______, KC_LCTL, KC_LALT, KC_DOT,  _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_RSFT
+    _______, KC_0,    KC_LALT, KC_DOT,  _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_RSFT
 ),
 
 /* Plover layer (http://opensteno.org)
