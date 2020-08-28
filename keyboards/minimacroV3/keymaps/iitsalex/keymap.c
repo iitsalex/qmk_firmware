67e394f0a1edf441916b34bc531d8bc1dcaa3cbd
Alex Khalilieh <iitsalex@hotmail.com>
2020-08-27 21:47:19 -0700

    Adding tap dancing

--- keyboards/minimacroV3/keymaps/iitsalex/keymap.c
+++ keyboards/minimacroV3/keymaps/iitsalex/keymap.c
@@ -7,6 +7,17 @@ int increment = 0;
 #define _MAIN 0
 #define _SPOTIFY 1
 
+enum {
+    TD_DISCORD,
+    TD_SPOTIFY
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    // Tap once for Escape, twice for Caps Lock
+    [TD_DISCORD] = ACTION_TAP_DANCE_DOUBLE(LGUI(KC_4), LALT(KC_TAB)),
+    [TD_SPOTIFY] = ACTION_TAP_DANCE_DOUBLE(LGUI(KC_5), LALT(KC_TAB))
+};
+
 void encoder_update_user(uint8_t index, bool clockwise) {
     switch(biton32(layer_state)){
         case 1:
@@ -47,9 +58,9 @@ void encoder_update_user(uint8_t index, bool clockwise) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { //buttion closest to usb is first
   [_MAIN] = LAYOUT(
-    TO(_SPOTIFY), LGUI(KC_5), LGUI(KC_4), KC_MNXT, KC_MPLY, KC_MPRV
+    TO(_SPOTIFY), TD(TD_SPOTIFY), TD(TD_DISCORD), KC_MNXT, KC_MPLY, KC_MPRV
   ),
   [_SPOTIFY] = LAYOUT(
-    TO(_MAIN), LGUI(KC_5), LGUI(KC_4), KC_MNXT, KC_MPLY, KC_MPRV
+    TO(_MAIN), TD(TD_SPOTIFY), TD(TD_DISCORD), KC_MNXT, KC_MPLY, KC_MPRV
   )
 };
